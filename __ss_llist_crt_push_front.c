98df897e0bc4454bb86ab722c0e7737c4a06b028
ssokar-cpu <ssokar.ssokar@protonmail.com>
2022-11-20 11:16:29 +0100

    [LIBFT] FIX: Corrects the segmentation fault when the list is empty.

--- __ss_llist_crt_push_front.c
+++ __ss_llist_crt_push_front.c
@@ -7,9 +7,17 @@ t_list	*__ss_llist_crt_push_front( t_llist_crt **llist, void *content, size_t co
 	t_list *new = ft_lstnew( content, content_size );
 	if ( new == NULL )
 		goto __ss_llist_crt_push_front_err;
-	new->next = (*llist)->llist_begin;
-	(*llist)->llist_begin->prev = new;
-	(*llist)->llist_begin = new;
+	if ( (*llist)->llist_last == NULL  && (*llist)->llist_begin == NULL )
+	{
+		(*llist)->llist_begin = new;
+		(*llist)->llist_last = new;
+	}
+	else
+	{
+		new->next = (*llist)->llist_begin;
+		(*llist)->llist_begin->prev = new;
+		(*llist)->llist_begin = new;
+	}
 	
 	( callback != NULL ) ? ( void )callback( (*llist)->content ) : 0X00;
 	return ( new );
--- __ss_push_back.c
+++ __ss_push_back.c
@@ -7,9 +7,17 @@ t_list	*__ss_llist_crt_push_back( t_llist_crt **llist, void *content, size_t con
 	t_list *new = ft_lstnew( content, content_size );
 	if ( new == NULL )
 		goto __ss_llist_crt_push_back_err;
-	(*llist)->llist_last->next = new;
-	new->prev = (*llist)->llist_last;
-	(*llist)->llist_last = new;
+	if ( (*llist)->llist_last == NULL  && (*llist)->llist_begin == NULL )
+	{
+		(*llist)->llist_begin = new;
+		(*llist)->llist_last = new;
+	}
+	else
+	{
+		(*llist)->llist_last->next = new;
+		new->prev = (*llist)->llist_last;
+		(*llist)->llist_last = new;
+	}
 
 	( callback != NULL ) ? ( void )callback( (*llist)->content ) : 0X00;
 __ss_llist_crt_push_back_err:
