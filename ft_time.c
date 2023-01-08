8b4ba546f4374207c4ed31a7667883986197031a
Romain CARETTE <r.carette@protonmail.ch>
2023-01-08 06:09:59 +0100

    [LIBFT] [FT_TIME] FIX: Deletion of brackets

--- ft_time.c
+++ ft_time.c
@@ -3,5 +3,5 @@
 void	_time( char *str ) {
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
-	sprintf(str, "[%d-%02d-%02d %02d:%02d:%02d]", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	sprintf(str, "%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 }
