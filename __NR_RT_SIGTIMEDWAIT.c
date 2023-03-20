b28d54e001830a7c94eaf606898b76b8d09330b2
Romain CARETTE <r.carette@protonmail.ch>
2023-03-20 19:49:28 +0100

    [SYSCALL] FIX: rt_sigtimedwait functions written in assembler

--- __NR_RT_SIGTIMEDWAIT.c
+++ __NR_RT_SIGTIMEDWAIT.c
@@ -3,9 +3,9 @@
 int	__NR_RT_SIGTIMEDWAIT( const sigset_t *set, siginfo_t *info, const struct timespec *timeout, size_t sigsetsize )
 {
 	int	ret;
-	register long r8 asm("r8") = sigsetsize;
+	register long r10 asm("r10") = sigsetsize;
 	__asm__ volatile ("syscall" : "=a" (ret) : "a" (__NR_rt_sigtimedwait),
-		      "D" (set), "S" (info), "d" (timeout), "r" (r8) : 
+		      "D" (set), "S" (info), "d" (timeout), "r" (r10) : 
 		      "cc", "memory", "rcx", "r11");
 	return ( ret );
 }
